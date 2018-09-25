3008b4a24c8f35b90fd85aa9494375b5d69ee0d6
Zhilin Fu ( Alan Fu ) <alanfu@live.com>
2018-09-25 11:02:07 -0700

    Lastest Ver.

---
+++ controller.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+module.exports = {
+  waveformPlayer: function(req, res) {
+    axios(`http://18.219.124.16/api/waveformplayer/${req.params.id}`)
+    .then(function(response) {
+     res.send(response.data)
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
+  }
+}
--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const path = require('path');
 const axios = require('axios');
+const routes = require('./routes.js');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use('/songs/:id', express.static(path.join(__dirname, 'public')));
+app.use('/api', routes);
 /* * * * * * * * * * * For SocialArea * * * * * * * * * * */
 app.get('/api/stats/:id', (req, res) =>{
-  axios.get(`http://localhost:3004/api/stats/${req.params.id}`)
+  axios.get(`http://server-env-1.phjpybupp3.us-west-1.elasticbeanstalk.com/api/stats/${req.params.id}`)
   .then(({ data }) => {
     res.json(data);
   })
@@ -20,7 +22,7 @@ app.get('/api/stats/:id', (req, res) =>{
 app.get('/relatedTracks/:id', (req, res) => {
  // res.send(req.params.id)
  const songId = req.params.id;
- axios.get(`http://localhost:3002/relatedTracks/${songId}`)
+ axios.get(`http://18.219.127.175/relatedTracks/${songId}`)
    .then(({ data }) => {
      console.log('Related Tracks', data);
      res.json(data);
@@ -32,7 +34,7 @@ app.get('/relatedTracks/:id', (req, res) => {
 
 app.get('/relatedAlbums/:id', (req, res) => {
  const songId = req.params.id;
- axios.get(`http://localhost:3002/relatedAlbums/${songId}`)
+ axios.get(`http://18.219.127.175/relatedAlbums/${songId}`)
    .then(({ data }) => {
      console.log('Related Albums', data);
      res.json(data);
@@ -41,7 +43,18 @@ app.get('/relatedAlbums/:id', (req, res) => {
      console.log(error);
    });
 });
-//
+/* * * * * * * * * * * For Comment-Section * * * * * * * * * * */
+app.get('/api/comments/:songid', (req, res) => {
+  // console.log(`http:/localhost:3001/api/${req.params.songid}`);
+  axios.get(`http://comments-server.2u82f9p8mx.us-east-2.elasticbeanstalk.com/api/comments/${req.params.songid}`)
+    .then(function (response) {
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+/* * * * * * * * * * * For Waveform-Player * * * * * * * * * * */
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
